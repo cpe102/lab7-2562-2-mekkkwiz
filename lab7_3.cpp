eb09680a97f9f5f31387d3f5a0ca79ef857dedb3
mek <themakza0021@gmail.com>
2020-01-21 15:40:28 +0700

    doneeeee

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,65 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+int main()                              
+{
+    string status;
+    int age,high,money;
+    cout << "How old are you?: ";
+    cin >> age;
+    if (age <= 20)
+    {
+        cout << "How tall are you?: ";
+        cin >> high;
+        if (high < 160)
+        {
+            status = "UNFRIEND";
+        }
+        else if (high < 175)
+        {
+            status = "FRIEND";
+        }
+        else
+        {
+            cout << "How much do you have asset?: ";
+            cin >> money;
+            if (money > 69e7)
+            {
+                status = "MARRIED";
+            }
+            else
+            {
+                status = "ONE-NIGHT-STAND";
+            }
+            
+            
+        }
+        
+        
+        
+
+    }
+    else if (age < 30)
+    {
+        cout << "How much do you have asset?: ";
+        cin >> money;
+        if (money > 10e7)
+            {
+                status = "BEST FRIND";
+            }
+            else
+            {
+                status = "UNFRIEND";
+            }
+    }
+    else
+    {
+        status = "UNFRIEND";
+    }
+    
+
+    cout << status;                                        
+}
+   
+    
